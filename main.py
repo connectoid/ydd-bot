99490c235117c991aabed7db1540c73dc7a66d32
Alexander Beley <beleysasha@gmail.com>
2024-01-11 21:53:35 +1200

    file size limit

--- main.py
+++ main.py
@@ -18,29 +18,36 @@ dp = Dispatcher()
 BASE_URL = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
 TEST_URL = 'https://disk.yandex.ru/d/-kncz6cshtxBlQ'
 TEST_URL = 'https://disk.yandex.ru/i/yBRtFPHfjceDxw'
-ERRORS = ['WRONG LINK', 'CANT DOWNLOAD']
+ERRORS = ['WRONG LINK', 'CANT DOWNLOAD', 'FILE IS TOO BIG']
+MAX_FILE_SIZE = 52428800
 
 def download_file(url):
     # Получаем загрузочную ссылку
     final_url = BASE_URL + urlencode(dict(public_key=url))
     response = requests.get(final_url)
     if response.status_code == 200:
-        print(url)
-        filename = response.json()['href'].split('&')[1].split('=')[1]
-        filename = unquote(filename)
-        print(filename)
-        download_url = response.json()['href']
+        print(response.json())
+        filesize = response.json()['href'].split('fsize=')[1].split('&')[0]
+        if int(filesize) < MAX_FILE_SIZE:
+            print(filesize)
+            filename = response.json()['href'].split('&')[1].split('=')[1]
+            filename = unquote(filename)
+            print(filename)
+            download_url = response.json()['href']
 
-        # Загружаем файл и сохраняем его
-        download_response = requests.get(download_url)
-        if download_response.status_code == 200:
-            print(download_response)
-            with open(filename, 'wb') as f:   # Здесь укажите нужный путь к файлу
-                f.write(download_response.content)
-            return filename
-        else: 
-            print('CANT DOWNLOAD')
-            return 'CANT DOWNLOAD'
+            # Загружаем файл и сохраняем его
+            download_response = requests.get(download_url)
+            if download_response.status_code == 200:
+                print(download_response)
+                with open(filename, 'wb') as f:   # Здесь укажите нужный путь к файлу
+                    f.write(download_response.content)
+                return filename
+            else: 
+                print('CANT DOWNLOAD')
+                return 'CANT DOWNLOAD'
+        else:
+            print('FILE IS TOO BIG')
+            return 'FILE IS TOO BIG'
     else:
         print('WRONG LINK')
         return 'WRONG LINK'
@@ -55,7 +62,7 @@ async def send_echo(message: Message):
     # await message.reply(message.text)
     filename = download_file(message.text)
     if filename not in ERRORS:
-        await message.reply(f'Файл {filename} готов к скачиванию:')
+        await message.reply(f'Файл {filename} готов к скачиванию, ожидайте')
         send_file = FSInputFile(filename)
         await message.reply_document(send_file)
     else:
