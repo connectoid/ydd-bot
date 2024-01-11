86f9550202657210643163d9ebce9c9ee26afb80
Alexander Beley <beleysasha@gmail.com>
2024-01-11 20:09:58 +1200

    small files done

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 import dotenv
 import requests
-from urllib.parse import urlencode
+from urllib.parse import urlencode, unquote
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.types import FSInputFile
 
 dotenv.load_dotenv()
 
@@ -17,6 +18,7 @@ dp = Dispatcher()
 BASE_URL = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
 TEST_URL = 'https://disk.yandex.ru/d/-kncz6cshtxBlQ'
 TEST_URL = 'https://disk.yandex.ru/i/yBRtFPHfjceDxw'
+ERRORS = ['WRONG LINK', 'CANT DOWNLOAD']
 
 def download_file(url):
     # Получаем загрузочную ссылку
@@ -24,19 +26,24 @@ def download_file(url):
     response = requests.get(final_url)
     if response.status_code == 200:
         print(url)
-        print(response)
+        filename = response.json()['href'].split('&')[1].split('=')[1]
+        filename = unquote(filename)
+        print(filename)
         download_url = response.json()['href']
 
         # Загружаем файл и сохраняем его
         download_response = requests.get(download_url)
         if download_response.status_code == 200:
             print(download_response)
-            with open('downloaded_file.txt', 'wb') as f:   # Здесь укажите нужный путь к файлу
+            with open(filename, 'wb') as f:   # Здесь укажите нужный путь к файлу
                 f.write(download_response.content)
+            return filename
         else: 
             print('CANT DOWNLOAD')
+            return 'CANT DOWNLOAD'
     else:
         print('WRONG LINK')
+        return 'WRONG LINK'
 
 @dp.message(Command(commands=['start']))
 async def proccess_start_command(message: Message):
@@ -45,8 +52,15 @@ async def proccess_start_command(message: Message):
     
 @dp.message()
 async def send_echo(message: Message):
-    await message.reply(message.text)
-    download_file(message.text)
+    # await message.reply(message.text)
+    filename = download_file(message.text)
+    if filename not in ERRORS:
+        await message.reply(f'Файл {filename} готов к скачиванию:')
+        send_file = FSInputFile(filename)
+        await message.reply_document(send_file)
+    else:
+        await message.reply(filename)
+
 
 if __name__ == '__main__':
     dp.run_polling(bot)
